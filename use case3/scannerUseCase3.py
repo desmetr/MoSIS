90576a2f676395d502f2fd06c7e9103cfbc8d905
desmetr <rafael.de.smet@gmail.com>
2018-10-16 13:17:01 +0200

    use case 3 test trace OK + finetuned FSA

--- use case3/scannerUseCase3.py
+++ use case3/scannerUseCase3.py
@@ -9,8 +9,8 @@ False = 0
 True  = 1
 
 # trace FSA dynamics (True | False)
-# __trace__ = False 
-__trace__ = True 
+__trace__ = False 
+# __trace__ = True 
 
 class Scanner:
 	"""
@@ -21,6 +21,8 @@ class Scanner:
 		self.set_stream(stream)
 		self.current_state=None
 		self.accepting_states=[]
+
+		self.current_char = None
 
 	def set_stream(self, stream):
 		self.stream = stream
@@ -34,9 +36,11 @@ class Scanner:
 		while 1:
 			# look ahead at the next character in the input stream
 			next_char = self.stream.getNextChar()
+			self.current_char = next_char
 
 			# stop if this is the end of the input stream
-			if next_char == None: 
+			if next_char == None:
+				self.current_state = "S15"
 				break
 
 			if __trace__:
@@ -75,240 +79,6 @@ class Scanner:
 			self.stream.rollback()
 		return success
 
-## An example scanner, see http://moncs.cs.mcgill.ca/people/hv/teaching/SoftwareDesign/COMP304B2003/assignments/assignment3/solution/
-class NumberScanner(Scanner):
-	def __init__(self, stream):
-   		# superclass constructor
-		Scanner.__init__(self, stream)
-
-		# define accepting states
-		self.accepting_states=["S2","S4","S7"]
-
-	def __str__(self):
-  		return str(self.value)+"E"+str(self.exp)
-
-	def transition(self, state, input):
-		"""
-		Encodes transitions and actions
-		"""
-		if state == None:
-			# action
-			# initialize variables 
-			self.value = 0 
-			self.exp = 0
-			# new state
-			return "S1"
-
-		elif state == "S1":
-			if input  == '.':
-				# action
-				self.scale = 0.1
-				# new state
-				return "S3"
-   			elif '0' <= input <= '9':
-				# action
-				self.value = ord(string.lower(input))-ord('0')
-				# new state
-				return "S2"
-			else:
-				return None
-
-		elif state == "S2":
-			if input  == '.':
-				# action
-				self.scale = 0.1
-				# new state
-				return "S4"
-			elif '0' <= input <= '9':
-				# action
-				self.value = self.value*10+ord(string.lower(input))-ord('0')
-				# new state
-				return "S2"
-			elif string.lower(input)  == 'e':
-				# action
-				self.exp = 1
-				# new state
-				return "S5"
-			else:
-				return None
-
-		elif state == "S3":
-			if '0' <= input <= '9':
-				# action
-				self.value += self.scale*(ord(string.lower(input))-ord('0'))
-				self.scale /= 10
-				# new state
-				return "S4"	
-			else:
-				return None
-
-		elif state == "S4":
-			if '0' <= input <= '9':
-				# action
-				self.value += self.scale*(ord(string.lower(input))-ord('0'))
-				self.scale /= 10
-				# new state
-				return "S4"
-			elif string.lower(input)  == 'e':
-				# action
-				self.exp = 1
-				# new state
-				return "S5"
-			else:
-				return None
-
-		elif state == "S5":
-			if input  == '+':
-				# new state
-				return "S6"
-			elif input == '-':
-				# action
-				self.exp = -1
-				# new state
-				return "S6"
-			elif '0' <= input <= '9':
-				# action
-				self.exp *= ord(string.lower(input))-ord('0')
-				# new state
-				return "S7"
-			else:
-				return None
-
-		elif state == "S6":
-			if '0' <= input <= '9':
-				# action
-				self.exp *= ord(string.lower(input))-ord('0')
-				# new state
-				return "S7"
-			else:
-				return None
-
-		elif state == "S7":
-			if '0' <= input <= '9':
-				# action
-				self.exp = self.exp*10+ord(string.lower(input))-ord('0')
-				# new state
-				return "S7"
-			else:
-				return None
-
-		else:
-			return None
-
-	def entry(self, state, input):
-		pass
-
-## An example scanner, see http://msdl.cs.mcgill.ca/people/hv/teaching/SoftwareDesign/COMP304B2003/assignments/assignment3/solution/
-class CellRefScanner(Scanner):
-	def __init__(self, stream):
-		# superclass constructor
-		Scanner.__init__(self, stream)
-
-		# define accepting states
-		self.accepting_states=["S6","S7","S8", "S9"]
-
-	def __str__(self):
-		rowstr = str(self.row)
-		columnstr = str(self.column)
-		if self.rowIsAbsolute:
-			rowabsstr = "ABS_"
-		else:
-			rowabsstr = "REL_"
-		if self.columnIsAbsolute:
-			colabsstr = "ABS_"
-		else:
-			colabsstr = "REL_"
-		return "("+colabsstr+columnstr+","+rowabsstr+rowstr+")"
-
-	def transition(self, state, input):
-		"""
-		Encodes transitions and actions
-		"""
-		if state == None:
-			# action
-			# initialize variables 
-			self.row = 0 
-			self.rowIsAbsolute = False
-			self.column = 0
-			self.columnIsAbsolute = False
-			# new state
-			return "S1"
-
-		elif state == "S1":
-			if input == "$":
-				# action
-				self.columnIsAbsolute = True
-				# new state
-				return "S2"
-			elif input.isalpha():
-				return "S3"
-			else:
-				return None
-
-		elif state == "S2":
-			if input.isalpha():
-				return "S3"
-			else:
-				return None
-
-		elif state == "S3":
-			if '1' <= input <= '9':
-				return "S6"
-			elif input == "$":
-				return "S5"
-			elif input.isalpha():
-				self.column = 26*self.column + ord(string.lower(input)) - ord('a') + 1
-				return "S4"
-			else:
-				return None
-
-		elif state == "S4":
-			if input == "$":
-				return "S5"
-			elif '1' <= input <= '9':
-				return "S6" 
-			else:
-				return None
-
-		elif state == "S5":
-			if '1' <= input <= '9':
-				return "S6"
-			else:
-				return None
-
-		elif state == "S6":
-			if '0' <= input <= '9':
-				self.row = self.row*10 + ord(string.lower(input)) - ord('0')
-				return "S7"
-			else:
-				return None
-
-		elif state == "S7":
-			if '0' <= input <= '9':
-				self.row = self.row*10 + ord(string.lower(input)) - ord('0')
-				return "S8"
-			else:
-				return None 
-
-		elif state == "S8":
-			if '0' <= input <= '9':
-				self.row = self.row*10 + ord(string.lower(input)) - ord('0')
-				return "S9"
-			else:
-				return None
-
-		elif state == "S9":
-			return None
-
-	def entry(self, state, input):
-		if state == "S3":
-			self.column = ord(string.lower(input)) - ord('a') + 1
-		elif state == "S5":
-			self.rowIsAbsolute = True
-		elif state == "S6":
-			self.row = ord(string.lower(input)) - ord('0')
-
-
 class UseCase3Scanner(Scanner):
 	def __init__(self, stream):
    		# superclass constructor
@@ -318,21 +88,21 @@ class UseCase3Scanner(Scanner):
 		self.alphabet = ['E', 'R', 'G', 'X', '1', '2', '3', ' ']
 
 		# define accepting states
-		self.accepting_states=["S14"]
+		self.accepting_states=["S14", "S15"]
 
 	def __str__(self):
-  		return self.string
+  		return self.result
 
   	def transition(self, state, input):
 		"""
 		Encodes transitions and actions
 		"""
-
 		if state == None:
-			self.string = ""
+			self.result = ""
 			self.seenR1 = False
 			self.seenR2 = False
 			self.inComment = False
+			self.recognized = False
 			return "S0"
 
 		elif state == "S0":
@@ -375,7 +145,7 @@ class UseCase3Scanner(Scanner):
 			elif input == '\n':
 				self.inComment = False
 				return "S3"
-			elif input != 'R' and self.inComment:
+			elif self.inComment:
 				return "S3"
 			else:
 				return None
@@ -408,6 +178,8 @@ class UseCase3Scanner(Scanner):
 				return "S6"
 			elif self.inComment:
 				return "S6"
+			elif input in self.alphabet:
+				return "S6"
 			else:
 				return None
 
@@ -422,6 +194,8 @@ class UseCase3Scanner(Scanner):
 				self.inComment = False
 				return "S7"
 			elif self.inComment:
+				return "S7"
+			elif input in self.alphabet:
 				return "S7"
 			else:
 				return None
@@ -454,6 +228,8 @@ class UseCase3Scanner(Scanner):
 				return "S10"
 			elif self.inComment:
 				return "S10"
+			elif input in self.alphabet:
+				return "S10"
 			else:
 				return None
 
@@ -468,6 +244,8 @@ class UseCase3Scanner(Scanner):
 				self.inComment = False
 				return "S11"
 			elif self.inComment:
+				return "S11"
+			elif input in self.alphabet:
 				return "S11"
 			else:
 				return None
@@ -484,9 +262,28 @@ class UseCase3Scanner(Scanner):
 			else:
 				return None
 
+		# If we have recognized one statement, keep checking if there is more input
+		elif state == "S14":
+			if input != None:
+				self.recognized = True
+				return "S0"
+			else:
+				return None
+
 		else:
 			return None
 
 	def entry(self, state, input):
 		if input in self.alphabet and not self.inComment:
-			self.string += input
+			self.result += input
+		if input in ['1', '2', '3']:
+			self.result += " "
+
+		if self.recognized:
+			print ">> recognized " + self.__str__()
+			print ">> continuing reading input"
+			self.result = ""
+			self.seenR1 = False
+			self.seenR2 = False
+			self.inComment = False
+			self.recognized = False
