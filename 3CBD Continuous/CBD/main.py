3ce88013a8b0832efbe6c5de2dca080136d9035e
desmetr <rafael.de.smet@gmail.com>
2018-11-06 13:43:14 +0100

    simulink oscillator A ok, code almost

--- 3CBD Continuous/CBD/main.py
+++ 3CBD Continuous/CBD/main.py
@@ -2,15 +2,59 @@
 from bokeh.plotting import figure, output_file, show
 from CBDMultipleOutput.Source.CBD import *
 from CBDMultipleOutput.Source.CBDDraw import draw
-from CBDMultipleOutput.Source.LatexWriter import *
 
-class Adder(CBD):
+# Harmonic oscillator using integral blocks
+class CBD_A(CBD):
     def __init__(self, block_name):
-        CBD.__init__(self, block_name, input_ports=[], output_ports=["OutDouble"])
-        self.addBlock(ConstantBlock(block_name="three", value=3.0))
-        self.addBlock(ConstantBlock(block_name="two", value=2.0))
-        self.addBlock(AdderBlock(block_name="adder"))
+        CBD.__init__(self, block_name, input_ports=[], output_ports=["OUT1"])
+        self.addBlock(ConstantBlock("Zero", 0))
+        self.addBlock(ConstantBlock("One", 1))
+        self.addBlock(ConstantBlock("DeltaT", 1))
+        self.addBlock(NegatorBlock("Negate")) 
+        self.addBlock(IntegratorBlock("Integrator1"))    # Handles highest order
+        self.addBlock(IntegratorBlock("Integrator2"))    # Handles lowest order
 
-        self.addConnection("three", "adder")
-        self.addConnection("two", "adder")
-        self.addConnection("adder", "OutDouble")
+        self.addBlock(DelayBlock("DelayDeltaT"))
+        self.addBlock(AdderBlock("AdderDeltaT"))
+
+        # DeltaT 
+        self.addConnection("AdderDeltaT", "DelayDeltaT")
+        self.addConnection("Zero", "DelayDeltaT", input_port_name="IC")
+
+        self.addConnection("DelayDeltaT", "AdderDeltaT")
+        self.addConnection("DeltaT", "AdderDeltaT")
+
+        # Oscillator
+        self.addConnection("One", "Integrator1", input_port_name="IC")
+        self.addConnection("Zero", "Integrator2", input_port_name="IC")
+        self.addConnection("AdderDeltaT", "Integrator1", input_port_name="delta_t")
+        self.addConnection("AdderDeltaT", "Integrator2", input_port_name="delta_t")
+        self.addConnection("Integrator1", "Integrator2")    # x'
+        self.addConnection("Integrator2", "Negate")         # -x
+        self.addConnection("Negate", "Integrator1")
+        self.addConnection("Integrator2", "OUT1", output_port_name="OUT1")    
+
+# Harmonic oscillator using derivative blocks
+class CBD_B(CBD):
+    def __init__(self, block_name):
+        CBD.__init__(self, block_name, input_ports=[], output_ports=["OUT1"])
+
+
+        
+cbdA = CBD_A("CBD_A")
+draw(cbdA, "CBD_A.dot")
+cbdA.run(10)
+
+times = []
+output = []
+
+for timeValuePair in cbdA.getSignal():
+    times.append(timeValuePair.time)
+    output.append(timeValuePair.value)
+
+print output, times
+
+#Plot
+# p = figure(title="Harmonic Oscillator 1", x_axis_label='time', y_axis_label='N')
+# p.circle(x=times, y=output)
+# show(p)
